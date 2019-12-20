2cbfb81a758b59bf42a8295eb7d0860c2151bc5f
Jibgeria <kimminsu65772@gmail.com>
2019-12-21 03:17:17 +0900

    정보를 받아 올 사이트를 설정함.

--- info.py
+++ info.py
@@ -10,3 +10,9 @@ LP = [] #리그 포인트
 Wins = [] #승리 횟수
 Losses = [] #패배 횟수
 Ratio = [] #승률
+
+url = 'https://www.op.gg/summoner/userName=' + Name
+hdr = {'Accept-Language': 'ko_KR,en;q=0.8', 'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')} 
+req = request.get(url, headers=hdr)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
