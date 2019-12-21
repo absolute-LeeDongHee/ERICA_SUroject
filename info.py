69a8fa8ef6ee0daf94038406716c8a01aa9bc1d7
Jibgeria <kimminsu65772@gmail.com>
2019-12-21 18:45:32 +0900

    이름, 랭킹, 리그, 티어 크롤링 작업

--- info.py
+++ info.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 Name = input("검색하실 닉네임을 입력하세요!")
-Nickname = ""
+SummonerName = ""
+Ranking = ""
 Unranked = ""
 LeagueType = [] #속한 리그 이름
 Tier = [] #티어
@@ -16,3 +17,18 @@ hdr = {'Accept-Language': 'ko_KR,en;q=0.8', 'User-Agent': ('Mozilla/5.0 (X11; Li
 req = request.get(url, headers=hdr)
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
+
+#닉네임 크롤링======================================================
+for i in soup.select('div[class=SummonerName]'):
+	SummonerName = i.text
+#랭킹 크롤링========================================================
+for i in soup.select('span[class=ranking]'):
+	Ranking = i.text
+#당신은 언랭?=======================================================
+Tier_Unranked = soup.select('div.cell')
+#속한 리그 크롤링===================================================
+for i in soup.select('div[class=LeagueType]'):
+	LeagueType.append(i.text)
+# 티어 크롤링=======================================================
+for i in soup.select('div[class=Tier]'):
+	Tier.append(i.text)
