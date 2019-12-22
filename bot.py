fbf47dea11e8e1d3863cd0f989f4ff4dc84415f0
Jibgeria <kimminsu65772@gmail.com>
2019-12-22 17:38:36 +0900

    롤을 하지 않았더라면 추가

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 client = discord.Client()
 
-token = 'NjU4MTY4NDk3MzYwMzM4OTU0.Xf71aw.nALPIfO53FQHPYN5sSj_rYMYn2I'
+token = 'Input your token'
 
 @client.event
 async def on_ready():
@@ -34,7 +34,7 @@ async def on_message(message):
 		embed.add_field(name='!관전',value = "실시간 프로게이머 관전 사이트를 알려줍니다.", inline=False)
 		embed.add_field(name='!전적 닉네임',value = "검색한 닉네임 유저의 전적을 알려줍니다.", inline=False)
 		embed.add_field(name='!시간 닉네임',value = "롤 플레이 시간을 알려줍니다.", inline=False)
-		embed.add_field(name='!시간낭비 닉네임',value = "롤을 안 했더라면 할 수 있었던 일을 알려줍니다.", inline=False)
+		embed.add_field(name='!낭비 닉네임',value = "롤을 안 했더라면 할 수 있었던 일을 알려줍니다.", inline=False)
 		embed.add_field(name='!랭킹 ',value = "롤 플레이 시간 랭킹을 확인할 수 있습니다.", inline=False)
 		await channel.send(embed=embed)
 
@@ -138,12 +138,36 @@ async def on_message(message):
 		soup = BeautifulSoup(html, 'html.parser')
 		# 소환사 이름 크롤링
 		summonername3 = soup.find(['h1'])
+		#랭킹 크롤
 		for i in soup.find_all('div', 'rank center'):
 			rank.append(i)
 		if summonername3 != "":
 			embed = discord.Embed(title="해당 소환사의 플레이 랭킹입니다. (랭킹 뒤의 영어는 해당 지역을 말합니다.)", color=0x00ff00)
 			embed.add_field(name='국내 랭킹', value=rank[0].get_text(strip=True), inline=False)
 			embed.add_field(name='세계 랭킹', value=rank[1].get_text(strip=True), inline=False)
+			await channel.send(embed=embed)
+		else:
+			await channel.send("소환사 정보가 없습니다.")
+
+	if message.content.startswith("!낭비"):
+		channel = message.channel
+		Name = message.content[4:]
+		fact = []
+		url = 'https://kr.wol.gg/stats/kr/' + Name + '/'
+		hdr = {'Accept-Language': 'ko_KR,en;q=0.8', 'User-Agent': (
+			'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Mobile Safari/537.36')}
+		req = requests.get(url, headers=hdr)
+		html = req.text
+		soup = BeautifulSoup(html, 'html.parser')
+		# 소환사 이름 크롤링
+		summonername4 = soup.find(['h1'])
+		for i in soup.find_all('div', 'fact center'):
+			fact.append(i)
+		if summonername4 != "":
+			embed = discord.Embed(title="그가 롤을 하지 않았더라면...", color=0x00ff00)
+			embed.add_field(name='독서', value=str(summonername4.get_text()) + "님은 " + str(fact[0].get_text()) + "를 할 수 있었습니다...", inline=False)
+			embed.add_field(name='영화 관람', value=str(summonername4.get_text()) + "님은 " + str(fact[1].get_text()) + "를 할 수 있었습니다...", inline=False)
+			embed.add_field(name='산책', value=str(summonername4.get_text()) + "님은 " + str(fact[1].get_text()) + "를 할 수 있었습니다...", inline=False)
 			await channel.send(embed=embed)
 		else:
 			await channel.send("소환사 정보가 없습니다.")
