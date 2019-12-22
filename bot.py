f5058e28b0843bf0e5b552bacf0e3de99a432c6d
Jibgeria <kimminsu65772@gmail.com>
2019-12-22 14:11:15 +0900

    디스코드 봇으로라도 만들어보자

---
+++ bot.py
@@ -0,0 +1,23 @@
+import asyncio
+import discord
+
+client = discord.Client()
+token = 'NjU4MTY4NDk3MzYwMzM4OTU0.Xf71aw.nALPIfO53FQHPYN5sSj_rYMYn2I'
+
+@client.event
+async def on_ready():
+	print("봇이 작동합니다!")
+	print(client.user.name)
+	print(client.user.id)
+
+@client. event
+# 봇 무시
+async def on_message(message):
+	if message.author.bot:
+		return None
+
+	if message.content.startswith("!안녕"):
+		channel = message.channel
+		await channel.send('ㅎㅇㅎㅇ!')
+
+client.run(token)
